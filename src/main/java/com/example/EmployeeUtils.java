c55d5f1d6b6e8f7ec8bc79d2983ada74b4cdaf46
Hari Prabhu Venkittaraman <maybehari@gmail.com>
2023-01-09 13:32:47 -0600

    lambdas

--- src/main/java/com/example/EmployeeUtils.java
+++ src/main/java/com/example/EmployeeUtils.java
@@ -6,27 +6,34 @@ import java.util.function.Predicate;
 public class EmployeeUtils {
 
   public String getFullName(Employee employee) {
-    Function<Employee, String> fullName = null;
+//    Function<x, y> = Accepts an arg and returns a result
+//    x = Type of input arg
+//    y = Type of return result
+    Function<Employee, String> fullName = (Employee e) -> e.getFirstName() + " " + e.getLastName();
     return fullName.apply(employee);
   }
 
   public String getManagersLastName(Employee employee) {
-    Function<Employee, String> managersLastName = null;
+    Function<Employee, String> managersLastName = (Employee e) -> e.getManager().getLastName();
     return managersLastName.apply(employee);
   }
 
   public boolean hasBeenEmployedLongerThanFiveYears(Employee employee) {
-    Predicate<Employee> employedLongerThanFiveYears = null;
+//    Predicate<x, y> = Accepts an arg and returns a boolean
+//    x = Type of input arg
+//    No need to specify type in the lambda Args - boolean checks do not need ternary operator statements
+//    Predicate<Employee> employedLongerThanFiveYears = (Employee e) -> e.getYearsOfService() > 5 ? true : false ;
+    Predicate<Employee> employedLongerThanFiveYears = e -> e.getYearsOfService() > 5;
     return employedLongerThanFiveYears.test(employee);
   }
 
   public boolean hasMoreThanThreeDirectReports(Employee employee) {
-    Predicate<Employee> moreThanThreeDirectReports = null;
+    Predicate<Employee> moreThanThreeDirectReports = e -> e.getNumberOfDirectReports() > 3;
     return moreThanThreeDirectReports.test(employee);
   }
 
   public boolean hasMoreThanThreeYearsOfServiceAndLessThanTwoDirectReports(Employee employee) {
-    Predicate<Employee> moreThanThreeYearsOfServiceAndLessThanTwoDirectReports = null;
+    Predicate<Employee> moreThanThreeYearsOfServiceAndLessThanTwoDirectReports = e -> e.getYearsOfService() > 3 && e.getNumberOfDirectReports() < 2;
     return moreThanThreeYearsOfServiceAndLessThanTwoDirectReports.test(employee);
   }
 }
